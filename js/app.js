5fedf66ce0c16b1548b2d443fdd5073713fb6c52
AngeloA1022 <angelo.armijo@inacapmail.cl>
2026-04-28 16:08:54 -0400

    prueba f

---
+++ js/app.js
@@ -0,0 +1,38 @@
+document.getElementById("registroForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const usuario = document.getElementById("usuario").value.trim();
+  const email = document.getElementById("emailRegistro").value.trim();
+  const password = document.getElementById("password").value;
+  const c_password = document.getElementById("c_password").value;
+
+  const error = document.getElementById("registroError");
+  const success = document.getElementById("registroSuccess");
+
+  error.textContent = "";
+  success.textContent = "";
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!usuario || !email || !password || !c_password) {
+    error.textContent = "Todos los campos son obligatorios";
+    return;
+  }
+
+  if (!emailRegex.test(email)) {
+    error.textContent = "Email inválido";
+    return;
+  }
+
+  if (password.length < 8) {
+    error.textContent = "La contraseña debe tener al menos 8 caracteres";
+    return;
+  }
+
+  if (password !== c_password) {
+    error.textContent = "Las contraseñas no coinciden";
+    return;
+  }
+
+  success.textContent = "Registro exitoso";
+});
